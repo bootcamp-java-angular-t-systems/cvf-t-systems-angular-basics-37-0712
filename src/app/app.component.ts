adcb5fb75888377b059e864d5f2cc912ec53ce91
carlavelasco15 <carlavelasco15@gmail.com>
2023-12-07 04:54:14 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,6 +9,37 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'UD37';
+
+export class AppComponent  {
+  title="UD37";
+  display: string = '';
+  operationComponents: string[] = [];
+
+  calculateResult(): void {
+    this.operationComponents.push(this.display);
+    var finalOperation = this.operationComponents.join('');
+    let resultString = eval(finalOperation).toString();
+    let truncatedResult = parseFloat(resultString).toFixed(3);
+    this.display = truncatedResult;
+    this.operationComponents = [];
+  }
+
+  printNumber(number: string): void {
+    this.display += number;
+  }
+  
+  deleteOne(): void {
+    this.display = this.display.slice(0, -1);
+  }
+
+  deleteAll(): void {
+    this.display = "";
+    this.operationComponents = [];
+  }
+
+  addOperand(operand: string): void {
+    this.operationComponents.push(this.display);
+    this.operationComponents.push(operand);
+    this.display = "";
+  }
 }
